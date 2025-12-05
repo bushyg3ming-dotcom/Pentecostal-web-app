f73adeb55580c712e4b3c49378f1ca2f2bf8ca37
MDKR3AP3R <riaanmwilliams@gmail.com>
2025-12-05 17:49:51 +0200

    i agree changes

--- pentecostal-app/src/App.js
+++ pentecostal-app/src/App.js
@@ -1038,7 +1038,7 @@ Email: pentecostalholychurch@gmail.com
                         type="checkbox"
                         required
                       />
-                      <span className="terms-text">
+                      <span className="terms-text" style={{whiteSpace: 'nowrap'}}>
                         I agree to the church's <a href="#privacy" style={{color: '#667eea'}}>privacy policy</a> and <a href="#terms" style={{color: '#667eea'}}>terms of service</a>.
                         {registrationForm.userType === 'pastor' &&
                           ' I understand that my information will be used to connect me with church members seeking spiritual guidance.'
